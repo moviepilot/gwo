b75d719e06fe9a575219d526bdb0d95c6322b079
Daniel Bornkessel <daniel@moviepilot.de>
2009-09-01 17:03:14 +0200

    'some' rewrites based on http://www.gwotricks.com/2009/05/server-side-dynamic-section-variations.html

--- lib/gwo.rb
+++ lib/gwo.rb
@@ -1,7 +1,10 @@
 module GWO
   module Helper
-    def gwo_start(id)
-      javascript_tag(%{
+    def gwo_start(id, *sections)
+      sections = ["gwo_section"] if sections.blank?
+        
+      src = %{
+        <script type="text/javascript">
         function utmx_section(){}function utmx(){}
         (function(){var k='#{id}',d=document,l=d.location,c=d.cookie;function f(n){
         if(c){var i=c.indexOf(n+'=');if(i>-1){var j=c.indexOf(';',i);return c.substring(i+n.
@@ -11,69 +14,65 @@ module GWO
         +'/siteopt.js?v=1&utmxkey='+k+'&utmx='+(x?x:'')+'&utmxx='+(xx?xx:'')+'&utmxtime='
         +new Date().valueOf()+(h?'&utmxhash='+escape(h.substr(1)):'')+
         '" type="text/javascript" charset="utf-8"></sc'+'ript>')})();
-      })
+        </script>
+      }
+      
+      sections.each do |section|
+        src += "<!-- utmx section name='#{section}' -->"
+      end
+      
+      src
     end
     
     def gwo_end(uacct, id)
-      javascript_tag(%{
-        if(typeof(urchinTracker)!='function')document.write('<sc'+'ript src="'+
-        'http'+(document.location.protocol=='https:'?'s://ssl':'://www')+
-        '.google-analytics.com/urchin.js'+'"></sc'+'ript>')
+      %{<script type="text/javascript">
+      if(typeof(_gat)!='object')document.write('<sc'+'ript src="http'+
+      (document.location.protocol=='https:'?'s://ssl':'://www')+
+      '.google-analytics.com/ga.js"></sc'+'ript>')</script>
+      <script type="text/javascript">
+      try {
+      var pageTracker=_gat._getTracker("#{uacct.inspect}");
+      pageTracker._trackPageview("/#{id}/test");
+      }catch(err){}</script>
+    }
+    
+    end
+    
+    def gwo_conversion(uacct, id)  
+      %{
+      <script type="text/javascript">
+      if(typeof(_gat)!='object')document.write('<sc'+'ript src="http'+
+      (document.location.protocol=='https:'?'s://ssl':'://www')+
+      '.google-analytics.com/ga.js"></sc'+'ript>')</script>
+      <script type="text/javascript">
+      try {
+      var pageTracker=_gat._getTracker("#{uacct.inspect}");
+      pageTracker._trackPageview("/#{id}/goal");
+      }catch(err){}</script>
+      }
+      
+    end
+
+    
+    def gwo_section(section = "gwo_section", variation_number = nil, &block)
+      if variation_number == nil
+        %{ <script>
+        var GWO_#{section} = utmx("variation_number", "#{section}");
+        if (GWO_#{section} != undefined && GWO_#{section} != 0) document.write('<no' + 'script>');
         </script>
-        <script>
-        try {
-        _uacct = #{uacct.inspect};
-        urchinTracker("/#{id}/test");
-        } catch (err) { }
-      })
-    end
-    
-    def gwo_conversion(uacct, id)
-      javascript_tag(%{
-        if(typeof(urchinTracker)!='function')document.write('<sc'+'ript src="'+
-        'http'+(document.location.protocol=='https:'?'s://ssl':'://www')+
-        '.google-analytics.com/urchin.js'+'"></sc'+'ript>')
-        </script>
-        <script>
-        try {
-        _uacct = #{uacct.inspect};
-        urchinTracker("/#{id}/goal");
-        } catch (err) { }
-      })
-    end
-    
-    def gwo_static_section(name, &block)
-      concat(script(name) { capture(&block) })
-    end
-    
-    def gwo_section(name, html_options = {}, &block)
-      concat(
-        content_tag(:div, 
-          capture(&block), 
-          html_options.merge({
-            :id    => "gwo_#{name.to_s}",
-            :style => "display:none"
-          })
-        )
-      )
-    end
-    
-    def gwo_dynamic_end(default, uacct, id)
-      javascript_tag(%{
-        function GWO(name){
-          document.getElementById("gwo_" + name).style.display = 'block';
+        #{capture(&block) if block_given?}
+        </noscript>
         }
-      }) + 
-      script(default) { 
-        javascript_tag("GWO(#{default.to_s.inspect})")
-      } + 
-      gwo_end(uacct, id)
+      else
+        %{<script>
+        if (GWO_#{section} == #{variation_number}) document.write('</noscript a="');
+        </script><!--">
+        #{capture(&block) if block_given?}
+        <script>document.write('<'+'!'+'-'+'-')</script>-->
+        }
+      end
     end
-    
-    def gwo(default, uacct, id)
-      gwo_start(id) + gwo_dynamic_end(default, uacct, id)
-    end
-    
+
     private
     
       # I'm overriding this since GWO doesn't like the CDATA section for some reason...
@@ -86,7 +85,7 @@ module GWO
             content_or_options_with_block
           end
         tag = content_tag(:script, content, html_options.merge(:type => Mime::JS))
-        if block_called_from_erb?(block)
+        if false # block_called_from_erb?(block)
           concat(tag)
         else
           tag
